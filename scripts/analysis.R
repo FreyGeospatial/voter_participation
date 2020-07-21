c7b062f64f87228487dfd882cc5582c646a5cb56
jmfrey <jfrey@clarku.edu>
2020-07-20 20:33:48 -0400

    created model. still need to validate and interperet results. Getting negative glm predict vals, so seems odd

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(eeptools)
+
 
 df <- read.csv("Data/ADAMS.txt")
 df$LAST_NAME <- NULL
@@ -49,7 +51,7 @@ for(j in 31:ncol(df)){
 df$YEAR_OF_BIRTH <- df$DATE_OF_BIRTH %>% lubridate::year()
 #df$AGE <- 2020 - df$AGE
 
-df$DATE_OF_BIRTH <- NULL
+# df$DATE_OF_BIRTH <- NULL
 df$RESIDENTIAL_ADDRESS1 <- NULL
 df$RESIDENTIAL_SECONDARY_ADDR <- NULL
 df$RESIDENTIAL_ZIP_PLUS4 <- NULL
@@ -80,44 +82,36 @@ for(i in 17:length(df)){
 
 df <- na.omit(df)
 
-df$AGE_2020 <- 2020 - df$YEAR_OF_BIRTH
+df$AGE_2019 <- 2019 - df$YEAR_OF_BIRTH
+df$AGE_GROUP_2019 <- NA
 for (i in 1:nrow(df)){
-  if(df$AGE_2020 >= 18 & df$AGE_2020 <= 21){
-    df$AGE_GROUP_2020 <- "18-21"
-  }else if(df$AGE_2020 >= 22 & df$AGE_2020 <= 29){
-    df$AGE_GROUP_2020 <- "22-29"
-  }else if(df$AGE_2020 >= 30 & df$AGE_2020 <= 39){
-    df$AGE_GROUP_2020 <- "30-39"
-  }else if(df$AGE_2020 >= 40 & df$AGE_2020 <= 49){
-    df$AGE_GROUP_2020 <- "40-49"
-  }else if(df$AGE_2020 >= 50 & df$AGE_2020 <= 59){
-    df$AGE_GROUP_2020 <- "50-59"
-  }else if(df$AGE_2020 >= 60 & df$AGE_2020 <= 69){
-    df$AGE_GROUP_2020 <- "60-69"
-  }else if(df$AGE_2020 >= 70 & df$AGE_2020 <= 79){
-    df$AGE_GROUP_2020 <- "70-79"
-  }else if(df$AGE_2020 >= 80 & df$AGE_2020 <= 89){
-    df$AGE_GROUP_2020 <- "80-89"
+  if(df$AGE_2019[i] >= 18 & df$AGE_2019[i] <= 21){
+    df$AGE_GROUP_2019[i] <- "18-21"
+  }else if(df$AGE_2019[i] >= 22 & df$AGE_2019[i] <= 29){
+    df$AGE_GROUP_2019[i] <- "22-29"
+  }else if(df$AGE_2019[i] >= 30 & df$AGE_2019[i] <= 39){
+    df$AGE_GROUP_2019[i] <- "30-39"
+  }else if(df$AGE_2019[i] >= 40 & df$AGE_2019[i] <= 49){
+    df$AGE_GROUP_2019[i] <- "40-49"
+  }else if(df$AGE_2019[i] >= 50 & df$AGE_2019[i] <= 59){
+    df$AGE_GROUP_2019[i] <- "50-59"
+  }else if(df$AGE_2019[i] >= 60 & df$AGE_2019[i] <= 69){
+    df$AGE_GROUP_2019[i] <- "60-69"
+  }else if(df$AGE_2019[i] >= 70 & df$AGE_2019[i] <= 79){
+    df$AGE_GROUP_2019[i] <- "70-79"
+  }else if(df$AGE_2019[i] >= 80 & df$AGE_2019[i] <= 89){
+    df$AGE_GROUP_2019[i] <- "80-89"
   }else{
-    df$AGE_GROUP_2020 <- "90+"
+    df$AGE_GROUP_2019[i] <- "90+"
   }
 }
 
-
-
-#reverse for loop: FILTER OUT VOTERS THAT WERE NOT ELIGIBLE TO VOTE IN 2000
-for (i in nrow(df):1){
-  if ((df$AGE[i] - 20) < 18){
-    df <- df[-i,]
-  }
-}
-
-
-#separate into primary and general election years
 general <- select(df,
                   VOTER_STATUS,
+                  DATE_OF_BIRTH,
                   YEAR_OF_BIRTH,
-                  AGE_2020,
+                  AGE_2019,
+                  AGE_GROUP_2019,
                   PARTY_AFFILIATION,
                   RESIDENTIAL_CITY,
                   #RESIDENTIAL_ZIP,
@@ -131,6 +125,76 @@ general <- select(df,
                   #STATE_SENATE_DISTRICT,
                   #TOWNSHIP,
                   contains("GENERAL"))
+
+general$Last4 <- (general$GENERAL.11.06.2018 + general$GENERAL.11.07.2017 +
+               general$GENERAL.11.08.2016 + general$GENERAL.11.03.2015)
+
+general$AGE_GROUP_2019 <- as.factor(general$AGE_GROUP_2019)
+
+general$DATE_OF_BIRTH <- as.Date(general$DATE_OF_BIRTH, "%Y-%m-%d")
+
+general$AGE_2015_GENERAL <- NA
+
+for (i in 1:nrow(general)){
+  if (age_calc(dob = general$DATE_OF_BIRTH[i], 
+               enddate = as.Date("2017-11-03", "%Y-%m-%d"),
+               units = "years") >= 18){
+    general$AGE_2015_GENERAL[i] <- "Eligible"
+  }else{
+    general$AGE_2015_GENERAL[i] <- "Not Eligible"
+  }
+}
+
+general <- general %>% 
+  filter(AGE_2015_GENERAL == "Eligible")
+
+#change this
+voter_turnout <- general %>% group_by(AGE_GROUP_2019) %>% summarise(turnout = mean(Last4))
+
+
+
+my_model <- glm(general$GENERAL.11.05.2019 ~ 
+      general$Last4 + 
+      general$AGE_2019 + 
+      general$PARTY_AFFILIATION + 
+      general$RESIDENTIAL_CITY)
+
+pred <- predict(my_model, type = "response")
+
+general$pred_2020 <- pred
+
+
+# #reverse for loop: FILTER OUT VOTERS THAT WERE NOT ELIGIBLE TO VOTE IN 2017
+# for (i in nrow(df):1){
+#   if ((df$AGE[i] - 3) < 18){
+#     df <- df[-i,]
+#   }
+# }
+
+
+# turnout percentage for each age group
+
+
+
+
+
+
+
+
+
+
+
+
+# #reverse for loop: FILTER OUT VOTERS THAT WERE NOT ELIGIBLE TO VOTE IN 2000
+# for (i in nrow(df):1){
+#   if ((df$AGE[i] - 20) < 18){
+#     df <- df[-i,]
+#   }
+# }
+
+
+#separate into primary and general election years
+
 
 
 
